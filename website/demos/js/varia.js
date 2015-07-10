9edca63416240d1fc50ef897354311bc52162a72
LinguList <mattis.list@uni-marburg.de>
2015-07-10 23:03:08 +0200

    updated console

---
+++ website/demos/js/varia.js
@@ -0,0 +1,21 @@
+function wind (country, season) {
+  /* Return the typical wind conditions for a country */
+
+  /* carry out type check of season, to set the basic value */
+  if (typeof season == "undefined") {
+    season = "summer";
+  }
+
+  if (country == "Germany" && season == "summer") {
+    return "There's strong wind in "+country+".";
+  }
+  else {
+    return "This part hasn't been programmed yet.";
+  }
+}
+
+function sw_align (seqA, seqB) {
+
+  return seqAlign(seqA.split(''), seqB.split(''));
+}
+
